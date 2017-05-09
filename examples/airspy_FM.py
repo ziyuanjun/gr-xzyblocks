a63aaba718c1973e45432e83374c067909edb02b
ziyuan <xzy_study@163.com>
2017-05-09 08:58:16 +0800

    Add FM receiver demos using airspy and rtl_sdr

--- examples/airspy_FM.py
+++ examples/airspy_FM.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Airspy Fm
-# Generated: Mon May  8 10:42:36 2017
+# Generated: Tue May  9 08:54:40 2017
 ##################################################
 
 if __name__ == '__main__':
@@ -16,12 +16,17 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
+from gnuradio import analog
+from gnuradio import audio
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
 from gnuradio.filter import firdes
-from gnuradio.wxgui import scopesink2
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import forms
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import osmosdr
@@ -39,28 +44,38 @@ class airspy_FM(grc_wxgui.top_block_gui):
         ##################################################
         # Variables
         ##################################################
+        self.freq_slider = freq_slider = 97.4e6
         self.samp_rate = samp_rate = 2500000
+        self.freq = freq = freq_slider
 
         ##################################################
         # Blocks
         ##################################################
-        self.wxgui_scopesink2_0 = scopesink2.scope_sink_c(
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
         	self.GetWin(),
-        	title='Scope Plot',
+        	baseband_freq=freq,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
         	sample_rate=samp_rate,
-        	v_scale=0,
-        	v_offset=0,
-        	t_scale=0,
-        	ac_couple=False,
-        	xy_mode=False,
-        	num_inputs=1,
-        	trig_mode=wxgui.TRIG_MODE_AUTO,
-        	y_axis_label='Counts',
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title='FFT Plot',
+        	peak_hold=False,
         )
-        self.Add(self.wxgui_scopesink2_0.win)
+        self.Add(self.wxgui_fftsink2_0.win)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=48,
+                decimation=500,
+                taps=None,
+                fractional_bw=None,
+        )
         self.osmosdr_source_1 = osmosdr.source( args="numchan=" + str(1) + " " + 'airspy' )
         self.osmosdr_source_1.set_sample_rate(samp_rate)
-        self.osmosdr_source_1.set_center_freq(97.4e6, 0)
+        self.osmosdr_source_1.set_center_freq(freq, 0)
         self.osmosdr_source_1.set_freq_corr(0, 0)
         self.osmosdr_source_1.set_dc_offset_mode(0, 0)
         self.osmosdr_source_1.set_iq_balance_mode(0, 0)
@@ -71,19 +86,71 @@ class airspy_FM(grc_wxgui.top_block_gui):
         self.osmosdr_source_1.set_antenna('', 0)
         self.osmosdr_source_1.set_bandwidth(0, 0)
 
+        self.low_pass_filter_0 = filter.fir_filter_ccf(4, firdes.low_pass(
+        	1, samp_rate, 100000, 1000000, firdes.WIN_HAMMING, 6.76))
+        _freq_slider_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._freq_slider_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_freq_slider_sizer,
+        	value=self.freq_slider,
+        	callback=self.set_freq_slider,
+        	label='freq',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._freq_slider_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_freq_slider_sizer,
+        	value=self.freq_slider,
+        	callback=self.set_freq_slider,
+        	minimum=80e6,
+        	maximum=110e6,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_freq_slider_sizer)
+        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=500000,
+        	audio_decimation=1,
+        )
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.osmosdr_source_1, 0), (self.wxgui_scopesink2_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.osmosdr_source_1, 0), (self.wxgui_fftsink2_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))
+
+    def get_freq_slider(self):
+        return self.freq_slider
+
+    def set_freq_slider(self, freq_slider):
+        self.freq_slider = freq_slider
+        self.set_freq(self.freq_slider)
+        self._freq_slider_slider.set_value(self.freq_slider)
+        self._freq_slider_text_box.set_value(self.freq_slider)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
         self.osmosdr_source_1.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 100000, 1000000, firdes.WIN_HAMMING, 6.76))
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.wxgui_fftsink2_0.set_baseband_freq(self.freq)
+        self.osmosdr_source_1.set_center_freq(self.freq, 0)
 
 
 def main(top_block_cls=airspy_FM, options=None):
--- examples/rtl_FM.py
+++ examples/rtl_FM.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 ##################################################
 # GNU Radio Python Flow Graph
-# Title: Fft Test
-# Generated: Fri May  5 21:52:00 2017
+# Title: Rtl Fm
+# Generated: Tue May  9 08:56:50 2017
 ##################################################
 
 if __name__ == '__main__':
@@ -16,13 +16,17 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
+from gnuradio import analog
+from gnuradio import audio
 from gnuradio import eng_notation
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
 from gnuradio.fft import window
 from gnuradio.filter import firdes
 from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import forms
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
 import osmosdr
@@ -30,24 +34,26 @@ import time
 import wx
 
 
-class fft_test(grc_wxgui.top_block_gui):
+class rtl_FM(grc_wxgui.top_block_gui):
 
     def __init__(self):
-        grc_wxgui.top_block_gui.__init__(self, title="Fft Test")
+        grc_wxgui.top_block_gui.__init__(self, title="Rtl Fm")
         _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
         self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
         # Variables
         ##################################################
+        self.freq_slider = freq_slider = 97.4e6
         self.samp_rate = samp_rate = 2e6
+        self.freq = freq = freq_slider
 
         ##################################################
         # Blocks
         ##################################################
         self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
         	self.GetWin(),
-        	baseband_freq=0,
+        	baseband_freq=freq,
         	y_per_div=10,
         	y_divs=10,
         	ref_level=0,
@@ -63,7 +69,7 @@ class fft_test(grc_wxgui.top_block_gui):
         self.Add(self.wxgui_fftsink2_0.win)
         self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + '' )
         self.rtlsdr_source_0.set_sample_rate(samp_rate)
-        self.rtlsdr_source_0.set_center_freq(97.4e6, 0)
+        self.rtlsdr_source_0.set_center_freq(freq, 0)
         self.rtlsdr_source_0.set_freq_corr(0, 0)
         self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
         self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
@@ -74,11 +80,60 @@ class fft_test(grc_wxgui.top_block_gui):
         self.rtlsdr_source_0.set_antenna('', 0)
         self.rtlsdr_source_0.set_bandwidth(0, 0)
 
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=48,
+                decimation=500,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.low_pass_filter_0 = filter.fir_filter_ccf(4, firdes.low_pass(
+        	1, samp_rate, 100000, 1000000, firdes.WIN_HAMMING, 6.76))
+        _freq_slider_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._freq_slider_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_freq_slider_sizer,
+        	value=self.freq_slider,
+        	callback=self.set_freq_slider,
+        	label='freq',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._freq_slider_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_freq_slider_sizer,
+        	value=self.freq_slider,
+        	callback=self.set_freq_slider,
+        	minimum=80e6,
+        	maximum=110e6,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_freq_slider_sizer)
+        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=500000,
+        	audio_decimation=1,
+        )
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.low_pass_filter_0, 0))
         self.connect((self.rtlsdr_source_0, 0), (self.wxgui_fftsink2_0, 0))
+
+    def get_freq_slider(self):
+        return self.freq_slider
+
+    def set_freq_slider(self, freq_slider):
+        self.freq_slider = freq_slider
+        self.set_freq(self.freq_slider)
+        self._freq_slider_slider.set_value(self.freq_slider)
+        self._freq_slider_text_box.set_value(self.freq_slider)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -87,9 +142,18 @@ class fft_test(grc_wxgui.top_block_gui):
         self.samp_rate = samp_rate
         self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
         self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 100000, 1000000, firdes.WIN_HAMMING, 6.76))
+
+    def get_freq(self):
+        return self.freq
+
+    def set_freq(self, freq):
+        self.freq = freq
+        self.wxgui_fftsink2_0.set_baseband_freq(self.freq)
+        self.rtlsdr_source_0.set_center_freq(self.freq, 0)
 
 
-def main(top_block_cls=fft_test, options=None):
+def main(top_block_cls=rtl_FM, options=None):
 
     tb = top_block_cls()
     tb.Start(True)
